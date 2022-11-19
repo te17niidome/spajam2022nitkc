b2bf12006d907eadceefc91e20c33ebe61bfb685
ae22niidome <ae22niidome@g.kumamoto-nct.ac.jp>
2022-11-20 03:56:59 +0900

    ジャッジとリザルト動くようにした

--- lib/judge.dart
+++ lib/judge.dart
@@ -1,17 +1,43 @@
+import 'dart:io';
+import 'dart:math' as math;
 import 'package:flutter/material.dart';
+import 'package:flutter_application_spajam2022/home.dart';
 import 'result.dart';
+import 'package:flutter_application_spajam2022/take_picture1.dart';
 
 class Judge extends StatelessWidget {
+  var image_win;
+  // 判定部分
+  void judgement() {
+    var random = math.Random();
+    double saikoro = random.nextDouble();
+    if (saikoro >= 0.5) {
+      image_win = image1;
+    } else {
+      image_win = image2;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    judgement();
     return Scaffold(
       appBar: AppBar(
         title: const Text('勝敗判定するよー'),
       ),
       body: Center(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('判定'),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(width: 160, child: Image.file(File(image1.path))),
+                Text(' VS ', style: TextStyle(fontSize: 20)),
+                SizedBox(width: 160, child: Image.file(File(image2.path))),
+              ],
+            ),
+            SizedBox(height: 100),
             ElevatedButton(
               onPressed: () {
                 Navigator.push(
@@ -22,7 +48,10 @@ class Judge extends StatelessWidget {
                   ),
                 );
               },
-              child: const Text('結果を見る'),
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: const Text('結果を見る', style: TextStyle(fontSize: 30)),
+              ),
             ),
           ],
         ),
--- lib/result.dart
+++ lib/result.dart
@@ -1,14 +1,52 @@
+import 'dart:io';
+import 'dart:math' as math;
 import 'package:flutter/material.dart';
+import 'package:flutter_application_spajam2022/home.dart';
+import 'package:flutter_application_spajam2022/take_picture1.dart';
+// import 'package:flutter_application_spajam2022/judge.dart';
 
-class Result extends StatelessWidget{
+class Result extends StatelessWidget {
+  var image_win;
+  // 判定部分
+  void judgement() {
+    var random = math.Random();
+    double saikoro = random.nextDouble();
+    if (saikoro >= 0.5) {
+      image_win = image1;
+    } else {
+      image_win = image2;
+    }
+  }
+
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
+    judgement();
     return Scaffold(
-      appBar:AppBar(
-        title: const Text('ホーム画面'),
+      appBar: AppBar(
+        title: const Text('Judgement Result'),
       ),
-      body:Center(
-        
+      body: Center(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Text('判定', style: TextStyle(fontSize: 50)),
+            ),
+            SizedBox(width: 360, child: Image.file(File(image_win.path))),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => Home(),
+                    fullscreenDialog: true,
+                  ),
+                );
+              },
+              child: const Text('ホームに戻る'),
+            ),
+          ],
+        ),
       ),
     );
   }
