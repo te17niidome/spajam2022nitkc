735d8ccd9345256646a1b979c8f7fcc719b8726a
yuhi25 <huru3334@gmail.com>
2022-11-19 21:49:51 +0900

    unchi

--- lib/main.dart
+++ lib/main.dart
@@ -1,54 +1,106 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
+import 'dart:ui' as ui;
 
-import 'camera.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/services.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:share_extend/share_extend.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: WidgetShareWidget(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class WidgetShareWidget extends StatelessWidget {
+  final GlobalKey shareKey = GlobalKey();
 
-  final String title;
+  Future<ByteData> exportToImage(GlobalKey globalKey) async {
+    final boundary =
+        globalKey.currentContext!.findRenderObject() as RenderRepaintBoundary;
+    final image = await boundary.toImage(
+      pixelRatio: 3,
+    );
+    final byteData = await image.toByteData(
+      format: ui.ImageByteFormat.png,
+    );
+    return byteData!;
+  }
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
+  Future<File> getApplicationDocumentsFile(
+      String text, List<int> imageData) async {
+    final directory = await getApplicationDocumentsDirectory();
 
-class _MyHomePageState extends State<MyHomePage> {
+    final exportFile = File('${directory.path}/$text.png');
+    if (!await exportFile.exists()) {
+      await exportFile.create(recursive: true);
+    }
+    final file = await exportFile.writeAsBytes(imageData);
+    return file;
+  }
+
+  void shareImageAndText(String text, GlobalKey globalKey) async {
+    //shareする際のテキスト
+    try {
+      //byte dataに
+      final bytes = await exportToImage(globalKey);
+      final widgetImageData =
+          bytes.buffer.asUint8List(bytes.offsetInBytes, bytes.lengthInBytes);
+      //App directoryファイルに保存
+      final applicationDocumentsFile =
+          await getApplicationDocumentsFile(text, widgetImageData);
+
+      final path = applicationDocumentsFile.path;
+      await ShareExtend.share(path, "image");
+      applicationDocumentsFile.delete();
+    } catch (error) {
+      print(error);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            ElevatedButton(
-              onPressed: () {
-                // ボタン押したときに呼ばれるコード
-                amain();
-              },
-              child: Text('ボタン'),
+      body: SafeArea(
+        child: Center(
+          child: SizedBox(
+            height: 200,
+            width: 300,
+            child: RepaintBoundary(
+              key: shareKey,
+              child: Card(
+                color: Colors.red,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Text(
+                      'Share this card!',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    ElevatedButton(
+                      onPressed: () => shareImageAndText(
+                        'sample_widget',
+                        shareKey,
+                      ),
+                      child: Text('Share'),
+                    ),
+                  ],
+                ),
+              ),
             ),
-          ],
+          ),
         ),
       ),
     );
