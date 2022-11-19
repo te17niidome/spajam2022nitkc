81e5b2131647c07e9248c987b2e2289b63cac8f3
ae22niidome <ae22niidome@g.kumamoto-nct.ac.jp>
2022-11-20 03:02:12 +0900

    kazunoko first commit

--- lib/judge.dart
+++ lib/judge.dart
@@ -1,24 +1,30 @@
 import 'package:flutter/material.dart';
 import 'result.dart';
 
-class Judge extends StatelessWidget{
+class Judge extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar:AppBar(
+      appBar: AppBar(
         title: const Text('勝敗判定するよー'),
       ),
-      body:Center(
-        child:ElevatedButton(
-          onPressed: (){
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => Result(),
-              fullscreenDialog: true,
-              ),
-            );
-          },
-          child: const Text('結果を見る'),
+      body: Center(
+        child: Column(
+          children: [
+            Text('判定'),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => Result(),
+                    fullscreenDialog: true,
+                  ),
+                );
+              },
+              child: const Text('結果を見る'),
+            ),
+          ],
         ),
       ),
     );
