dae3bfce0ba6c51b90bfb8e6ddbdf36faddeffd8
kazuma164 <yukukazu@gmail.com>
2022-11-20 10:37:22 +0900

    10:37

--- lib/judge.dart
+++ lib/judge.dart
@@ -26,37 +26,50 @@ class Judge extends StatelessWidget {
       appBar: AppBar(
         title: const Text('ホーム画面'),
       ),
-      body: Container(
-        width: 1000,
-        height: 2500,
-        decoration: const BoxDecoration(
-            image: DecorationImage(
-          image: AssetImage('images/判定.png'),
-          // fit: BoxFit.cover,
-        )),
-        child: Center(
-          child: SizedBox(
-            width: 220,
-            height: 250,
-            child: ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => Result(),
-                    fullscreenDialog: true,
+      body: Column(
+        children: [
+          Container(
+            width: 300,
+            height: 300,
+            decoration: const BoxDecoration(
+                image: DecorationImage(
+              image: AssetImage('images/判定.png'),
+              // fit: BoxFit.cover,
+            )),
+            child: Center(
+              child: SizedBox(
+                width: 220,
+                height: 250,
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => Result(),
+                        fullscreenDialog: true,
+                      ),
+                    );
+                  },
+                  child: const Text(''),
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.transparent,
+                    elevation: 0,
+                    onPrimary: Colors.blue,
                   ),
-                );
-              },
-              child: const Text(''),
-              style: ElevatedButton.styleFrom(
-                primary: Colors.transparent,
-                elevation: 0,
-                onPrimary: Colors.blue,
+                ),
               ),
             ),
           ),
-        ),
+          Container(
+            width: 510,
+            height: 400,
+            decoration: const BoxDecoration(
+                image: DecorationImage(
+              image: AssetImage('images/Videotogif(1).gif'),
+              // fit: BoxFit.cover,
+            )),
+          ),
+        ],
       ),
     );
   }
